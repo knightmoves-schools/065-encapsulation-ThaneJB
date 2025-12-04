1ddb5a893f401961b3477eeb9fae12e4a1af1eae
Thane <thabay0606@students.maquoketaschools.org>
2025-12-04 14:31:30 -0600

    Added Rectangle class with calculateArea method with no parameters.
    Refactored area calculation into Rectangle class.

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ let height = 10;
 function calculateArea(length, height){
     return length * height;
 }
+
+class Rectangle{
+    constructor(length, height){
+        this.length = length;
+        this.height = height;
+    }
+ calculateArea(){
+     return this.length * this.height;
+ }
+
+}
